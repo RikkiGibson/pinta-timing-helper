de81bcb7a2c29b77dcfa0b9d8974d049912e88b8
Rikki Gibson <rigibson@microsoft.com>
2024-12-14 11:29:45 -0800

    more

--- src/editor.ts
+++ src/editor.ts
@@ -8,7 +8,11 @@ function ready() {
 }
 
 async function onFileSelected(this: HTMLInputElement, ev: Event) {
-    const buffer = await this.files![0].arrayBuffer();
-    console.log(buffer.byteLength);
+    const bytes = new Uint8Array(await this.files![0].arrayBuffer());
+    console.log(bytes.length);
+    const filename = bytes.slice(0x200, 0x230);
+    const str = String.fromCharCode(...filename);
+    console.log(str);
+    // TODO: stick a button on the page to save the modified file
 }
 
