ede94d68e2dc0b11f521ebdab4e63cfe85ed3e6f
Rikki Gibson <rigibson@microsoft.com>
2024-12-14 11:19:27 -0800

    more

--- src/editor.ts
+++ src/editor.ts
@@ -3,4 +3,12 @@ export {};
 document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
+    const inputElement = document.getElementById('input-file')!;
+    inputElement.addEventListener('change', onFileSelected);
 }
+
+async function onFileSelected(this: HTMLInputElement, ev: Event) {
+    const buffer = await this.files![0].arrayBuffer();
+    console.log(buffer.byteLength);
+}
+
