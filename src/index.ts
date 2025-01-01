35fbff8f26eaf919a8a758755092f21374d474f6
Rikki Gibson <rigibson@microsoft.com>
2024-12-31 16:14:40 -0800

    adjust lastResetTime threshold

--- src/index.ts
+++ src/index.ts
@@ -450,7 +450,7 @@ function reset(manual: boolean) {
         }
 
         const currentTime = audioContext.currentTime;
-        if (lastResetTime !== undefined && currentTime - lastResetTime < beatTime * 3) {
+        if (lastResetTime !== undefined && currentTime - lastResetTime < beatTime * 4) {
             // Ignore an auto-reset if it came too soon after the most recent auto-reset.
             return;
         }
